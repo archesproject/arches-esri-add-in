4c2d5363b6cb93e4f8548f60a4ea8b69cab70b9a
njkim <nkim@fargeo.com>
2020-04-29 16:11:43 -0700

    Add a dockpane for creating a resource

---
+++ arches_arcgispro_addin/CreateResource.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace arches_arcgispro_addin
+{
+    /// <summary>
+    /// Interaction logic for CreateResourceView.xaml
+    /// </summary>
+    public partial class CreateResourceView : UserControl
+    {
+        public CreateResourceView()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
