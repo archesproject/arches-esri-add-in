7d3c556056466803d2ccccd4e1bb0223479c8ae3
njkim <nkim@fargeo.com>
2020-04-30 12:56:50 -0700

    Add a logic behind the create a resource

--- arches_arcgispro_addin/CreateResource.xaml.cs
+++ arches_arcgispro_addin/CreateResource.xaml.cs
@@ -1,8 +1,14 @@
-﻿using System;
+﻿using ArcGIS.Core.Geometry;
+using ArcGIS.Desktop.Editing.Attributes;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Mapping;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -26,18 +32,58 @@ namespace arches_arcgispro_addin
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private async void Button_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                if (MapView.Active == null)
+                {
+                    ArcGIS.Desktop.Framework.Dialogs.MessageBox.Show("No MapView currently active. Exiting...", "Info");
+                    return;
+                }
+                if (StaticVariables.myInstanceURL == "" | StaticVariables.myInstanceURL == null)
+                {
+                    ArcGIS.Desktop.Framework.Dialogs.MessageBox.Show("Please, Log in to Arches Server...");
+                    return;
+                }
+                if (StaticVariables.archesNodeid == "" | StaticVariables.archesNodeid == null)
+                {
+                    ArcGIS.Desktop.Framework.Dialogs.MessageBox.Show("Please, Select a Geometry Node to use...");
+                    return;
+                }
+                if (StaticVariables.archesResourceid != "" && StaticVariables.archesResourceid != null)
+                {
+                    StaticVariables.archesResourceid = "";
+                }
 
+                string archesGeometryString = await SaveResourceView.GetGeometryString();
+                string archesData = "data";
+                var result = await SaveResourceView.SubmitToArches("", StaticVariables.archesNodeid, archesData, archesGeometryString);
+                StaticVariables.archesResourceid = result["results"];
+                ArcGIS.Desktop.Framework.Dialogs.MessageBox.Show($"A resource id: \n{StaticVariables.archesResourceid} is assigned");
+                SaveResourceView.RefreshMapView();
+            }
+            catch (Exception ex)
+            {
+                ArcGIS.Desktop.Framework.Dialogs.MessageBox.Show("Exception: " + ex.Message);
+            }
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-
-        }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
+            if (StaticVariables.myInstanceURL == "" | StaticVariables.myInstanceURL == null)
+            {
+                ArcGIS.Desktop.Framework.Dialogs.MessageBox.Show("Please, Log in to Arches Server...");
+                return;
+            }
+            if (StaticVariables.archesResourceid == "" | StaticVariables.archesResourceid == null)
+            {
+                ArcGIS.Desktop.Framework.Dialogs.MessageBox.Show("Please, Submit a Geometry to Arches to Create a Resource...");
+                return;
+            }
+            string editorAddress = StaticVariables.myInstanceURL + $"resource/{StaticVariables.archesResourceid}";
+            ArcGIS.Desktop.Framework.Dialogs.MessageBox.Show("opening... \n" + editorAddress);
+            System.Diagnostics.Process.Start(editorAddress);
 
         }
     }
--- arches_arcgispro_addin/SaveResource.xaml.cs
+++ arches_arcgispro_addin/SaveResource.xaml.cs
@@ -24,7 +24,7 @@ using System.Windows.Shapes;
 namespace arches_arcgispro_addin
 {
     /// <summary>
-    /// Interaction logic for Dockpane1View.xaml
+    /// Interaction logic for SaveResourceView.xaml
     /// </summary>
     public partial class SaveResourceView : UserControl
     {
@@ -56,7 +56,7 @@ namespace arches_arcgispro_addin
             });
         }
 
-        private ArcGIS.Core.Geometry.Geometry SRTransform(ArcGIS.Core.Geometry.Geometry inGeometry, int inSRID, int outSRID)
+        private static ArcGIS.Core.Geometry.Geometry SRTransform(ArcGIS.Core.Geometry.Geometry inGeometry, int inSRID, int outSRID)
         {
             ArcGIS.Core.Geometry.Geometry outGeometry;
             SpatialReference inSR = SpatialReferenceBuilder.CreateSpatialReference(inSRID);
@@ -66,7 +66,7 @@ namespace arches_arcgispro_addin
             return outGeometry;
         }
 
-        private async Task<string> GetGeometryString()
+        public static async Task<string> GetGeometryString()
         {
             ArcGIS.Core.Geometry.Geometry archesGeometry;
             string archesGeometryString;
@@ -105,7 +105,7 @@ namespace arches_arcgispro_addin
             return args;
         }
 
-        private async Task<Dictionary<string, string>> SubmitToArches(string tileid, string nodeid, string data, string geojson)
+        public static async Task<Dictionary<string, string>> SubmitToArches(string tileid, string nodeid, string data, string geojson)
         {
             Dictionary<String, String> result = new Dictionary<String, String>();
 
@@ -134,7 +134,7 @@ namespace arches_arcgispro_addin
             return result;
         }
 
-        private async void RefreshMapView()
+        public static async void RefreshMapView()
         {
             await QueuedTask.Run(() =>
             {
@@ -142,7 +142,7 @@ namespace arches_arcgispro_addin
             });
         }
 
-            private void Button_Click(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
             // Check for an active mapview
             try
