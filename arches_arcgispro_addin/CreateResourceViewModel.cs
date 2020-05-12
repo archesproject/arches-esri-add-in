94052b9492bfc395317a464eff448d2015b26371
njkim <nkim@fargeo.com>
2020-05-11 23:50:27 -0700

    Add a dropdown of geometry nodeid to the ComboBox in CreateResource UI

--- arches_arcgispro_addin/CreateResourceViewModel.cs
+++ arches_arcgispro_addin/CreateResourceViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 using ArcGIS.Core.CIM;
 using ArcGIS.Core.Data;
 using ArcGIS.Core.Geometry;
@@ -15,7 +17,10 @@ using ArcGIS.Desktop.Framework.Contracts;
 using ArcGIS.Desktop.Framework.Dialogs;
 using ArcGIS.Desktop.Framework.Threading.Tasks;
 using ArcGIS.Desktop.Mapping;
-
+using ArcGIS.Desktop.Core.Events;
+using ArcGIS.Desktop.Mapping.Events;
+using System.Web.Script.Serialization;
+using System.Net.Http;
 
 namespace arches_arcgispro_addin
 {
@@ -23,7 +28,62 @@ namespace arches_arcgispro_addin
     {
         private const string _dockPaneID = "arches_arcgispro_addin_CreateResource";
 
-        protected CreateResourceViewModel() { }
+        private readonly object _lockCollection = new object();
+        public static ObservableCollection<GeometryNode> _geometryNodes = new ObservableCollection<GeometryNode>();
+        public ReadOnlyObservableCollection<GeometryNode> _readOnlyGeometryNodes;
+
+        protected CreateResourceViewModel() {
+            _readOnlyGeometryNodes = new ReadOnlyObservableCollection<GeometryNode>(_geometryNodes);
+            BindingOperations.EnableCollectionSynchronization(_readOnlyGeometryNodes, _lockCollection);
+            //CollectionChanged.Subscribe(OnProjectCollectionChanged, false);
+        }
+
+        /*private async void OnProjectCollectionChanged(ProjectItemsChangedEventArgs args)
+        {
+            _geometryNodes.Clear();
+            foreach (var geometryNode in StaticVariables.geometryNodes)
+            {
+                _geometryNodes.Add(geometryNode);
+            }
+        }*/
+
+        public static void GetGeometryNodes()
+        {
+            _geometryNodes.Clear();
+            foreach (var geometryNode in StaticVariables.geometryNodes)
+            {
+                _geometryNodes.Add(geometryNode);
+            }
+        }
+
+        public ReadOnlyObservableCollection<GeometryNode> ReadOnlyGeometryNodes => _readOnlyGeometryNodes;
+        public ObservableCollection<GeometryNode> GeometryNodes
+        {
+            set
+            {
+                SetProperty(ref _geometryNodes, value, () => GeometryNodes);
+                //NotifyPropertyChanged(() => GeometryNodes);
+            }
+
+            get { return _geometryNodes; }
+
+        }
+
+        private GeometryNode _selectedGeometryNode = new GeometryNode("");
+        public GeometryNode SelectedGeometryNode
+        {
+            get { return _selectedGeometryNode; }
+            set
+            {
+                SetProperty(ref _selectedGeometryNode, value, () => SelectedGeometryNode);
+                StaticVariables.archesNodeid = _selectedGeometryNode.Id;
+            }
+        }
+
+        public void AssignGeometryNodeid()
+        {
+            StaticVariables.archesNodeid = SelectedGeometryNode.Id;
+        }
 
         /// <summary>
         /// Show the DockPane.
